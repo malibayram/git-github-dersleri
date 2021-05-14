6f9b4059eda80e77c58f992554e6a3683e7f97ae
dershub <hubders@gmail.com>
2021-05-14 21:42:51 +0300

    ilk sayının büyük olması gerektiği kullanıya bildirildi

--- cikarma.js
+++ cikarma.js
@@ -1,3 +1,6 @@
 function cikar() {
-    sonuc.innerHTML = parseInt(sayi1.value) - parseInt(sayi2.value);
+    if (parseInt(sayi1.value) < parseInt(sayi2.value))
+	alert("çıkarma işlemi için birinci sayının ikinci sayıdan büyük olması gerekiyor!");
+    else
+        sonuc.innerHTML = parseInt(sayi1.value) - parseInt(sayi2.value);
 }
